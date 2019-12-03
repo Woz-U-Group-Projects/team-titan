ea65ab89c63e76903d11e621206cfce32349f6c6
jon-gerber <50384519+jon-gerber@users.noreply.github.com>
2019-12-02 18:05:58 -0800

    src/spp
    updated routing

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,24 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import { MatToolbarModule } from '@angular/material';
-
+import { MatToolbarModule,MatInputModule,
+        MatPaginatorModule,
+        MatProgressSpinnerModule,
+        MatSortModule,
+        MatTableModule,
+        MatIconModule,
+        MatButtonModule,
+        MatCardModule,
+        MatFormFieldModule } from '@angular/material';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ListComponent } from './components/list/list.component';
 import { CreateComponent } from './components/create/create.component';
 import { EditComponent } from './components/edit/edit.component';
-import { createComponent } from '@angular/compiler/src/core';
 import { IssueDetailsComponent } from './components/issue-details/issue-details.component';
 
 
 const routes: Routes = [
-  { path: 'create', component: CreateComponent},
-  { path: 'edit/:id', component: EditComponent},
-  { path: 'list', component: ListComponent},
+  { path: 'create', component: CreateComponent, data: {title: 'Create Issue'}},
+  { path: 'edit/:id', component: EditComponent, data: {title: 'Update Issue'}},
+  { path: 'list', component: ListComponent, data: {title: 'List Issues'}},
+  { path: 'issue-details/:id', component: IssueDetailsComponent, data: {title: 'Issue Details'}},
   { path: '', redirectTo: 'list', pathMatch: 'full'}
 ];  
 
@@ -38,7 +45,17 @@ const routes: Routes = [
     RouterModule.forRoot(routes),
     MatToolbarModule,
     FormsModule,
-    HttpClientModule
+    ReactiveFormsModule,
+    HttpClientModule,
+    MatInputModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatProgressSpinnerModule,
+    MatIconModule,
+    MatButtonModule,
+    MatCardModule,
+    MatFormFieldModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
